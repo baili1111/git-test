f12ca7f1fef8a1d218151c7bc6637b5693fa481b
zhuhaipeng <594010359@qq.com>
2021-09-05 16:14:22 +0800

    first commit

---
+++ src/main/java/com/xxx/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.xxx.git;
+
+/**
+ * @author zhu
+ * @date 2021/9/5 16:07:35
+ * @description
+ */
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+
+}
